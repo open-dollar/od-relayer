43b9432b5b8784ab7fa5a4111719912ce2d77bc4
Hunter King <hking6776@gmail.com>
2023-12-26 18:19:29 -0500

    rm enumerable sets

--- src/contracts/factories/CamelotRelayerFactory.sol
+++ src/contracts/factories/CamelotRelayerFactory.sol
@@ -1,22 +1,17 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.7.6;
 
-import {EnumerableSet} from '@openzeppelin/contracts/utils/EnumerableSet.sol';
 import {IBaseOracle} from '@interfaces/oracles/IBaseOracle.sol';
 import {CamelotRelayerChild} from '@contracts/factories/CamelotRelayerChild.sol';
 import {Authorizable} from '@contracts/utils/Authorizable.sol';
 
 contract CamelotRelayerFactory is Authorizable {
-  using EnumerableSet for EnumerableSet.AddressSet;
-
   uint256 public relayerId;
 
   // --- Events ---
   event NewAlgebraRelayer(address indexed _relayer, address _baseToken, address _quoteToken, uint32 _quotePeriod);
 
   // --- Data ---
-  // TODO: remove enumerable set?
-  EnumerableSet.AddressSet internal _relayers;
   mapping(uint256 => address) public relayerById;
 
   // --- Init ---
@@ -31,7 +26,6 @@ contract CamelotRelayerFactory is Authorizable {
     uint32 _quotePeriod
   ) external isAuthorized returns (IBaseOracle _relayer) {
     _relayer = IBaseOracle(address(new CamelotRelayerChild(_algebraV3Factory, _baseToken, _quoteToken, _quotePeriod)));
-    _relayers.add(address(_relayer));
     relayerId++;
     relayerById[relayerId] = address(_relayer);
     emit NewAlgebraRelayer(address(_relayer), _baseToken, _quoteToken, _quotePeriod);
--- src/contracts/factories/ChainlinkRelayerFactory.sol
+++ src/contracts/factories/ChainlinkRelayerFactory.sol
@@ -1,23 +1,18 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.7.6;
 
-import {EnumerableSet} from '@openzeppelin/contracts/utils/EnumerableSet.sol';
 import {IBaseOracle} from '@interfaces/oracles/IBaseOracle.sol';
 import {ChainlinkRelayerChild} from '@contracts/factories/ChainlinkRelayerChild.sol';
 import {Authorizable} from '@contracts/utils/Authorizable.sol';
 
 contract ChainlinkRelayerFactory is Authorizable {
-  using EnumerableSet for EnumerableSet.AddressSet;
-
   uint256 public relayerId;
 
   // --- Events ---
   event NewChainlinkRelayer(address indexed _chainlinkRelayer, address _aggregator, uint256 _staleThreshold);
 
-  mapping(uint256 => address) public relayerById;
-
   // --- Data ---
-  EnumerableSet.AddressSet internal _chainlinkRelayers;
+  mapping(uint256 => address) public relayerById;
 
   // --- Init ---
   constructor() Authorizable(msg.sender) {}
@@ -29,7 +24,6 @@ contract ChainlinkRelayerFactory is Authorizable {
     uint256 _staleThreshold
   ) external isAuthorized returns (IBaseOracle _chainlinkRelayer) {
     _chainlinkRelayer = IBaseOracle(address(new ChainlinkRelayerChild(_aggregator, _staleThreshold)));
-    _chainlinkRelayers.add(address(_chainlinkRelayer));
     relayerId++;
     relayerById[relayerId] = address(_chainlinkRelayer);
     emit NewChainlinkRelayer(address(_chainlinkRelayer), _aggregator, _staleThreshold);
