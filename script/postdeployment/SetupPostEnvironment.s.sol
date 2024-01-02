1a35a7c0751733d81ef1d088dad9df441d462241
Hunter King <hking6776@gmail.com>
2024-01-02 15:37:08 -0500

    fix merge conflict

--- script/postdeployment/SetupPostEnvironment.s.sol
+++ script/postdeployment/SetupPostEnvironment.s.sol
@@ -22,29 +22,16 @@ contract SetupPostEnvironment is Common {
   function run() public {
     vm.startBroadcast(vm.envUint('ARB_SEPOLIA_DEPLOYER_PK'));
 
-
     algebraFactory.createPool(SEPOLIA_SYSTEM_COIN, SEPOLIA_WETH);
     address _pool = algebraFactory.poolByPair(SEPOLIA_SYSTEM_COIN, SEPOLIA_WETH);
 
     uint160 _sqrtPriceX96 = initialPrice(INIT_OD_AMOUNT, INIT_WETH_AMOUNT, _pool);
     IAlgebraPool(_pool).initialize(uint160(_sqrtPriceX96));
 
-    // Todo: change to WETH for next deployment
     IBaseOracle _odWethOracle = camelotRelayerFactory.deployAlgebraRelayer(
-      SEPOLIA_ALGEBRA_FACTORY, SEPOLIA_SYSTEM_COIN, address(mockWeth), uint32(ORACLE_INTERVAL_TEST)
+      SEPOLIA_ALGEBRA_FACTORY, SEPOLIA_SYSTEM_COIN, SEPOLIA_WETH, uint32(ORACLE_INTERVAL_TEST)
     );
 
-    IBaseOracle chainlinkEthUSDPriceFeed =
-      chainlinkRelayerFactory.deployChainlinkRelayer(SEPOLIA_CHAINLINK_ETH_USD_FEED, ORACLE_INTERVAL_TEST);
-
-    // deploy systemOracle
-    denominatedOracleFactory.deployDenominatedOracle(_odWethOracle, chainlinkEthUSDPriceFeed, false);
-
-    revokeFactories();
-
-    /**
-     * note oracleRelayer will be set to systemOracle in odContracts post deploy script
-     * code: `oracleRelayer.modifyParameters('systemCoinOracle', abi.encode(systemCoinOracle));`
     IBaseOracle chainlinkEthUSDPriceFeed =
       chainlinkRelayerFactory.deployChainlinkRelayer(SEPOLIA_CHAINLINK_ETH_USD_FEED, ORACLE_INTERVAL_TEST);
 
@@ -60,40 +47,6 @@ contract SetupPostEnvironment is Common {
 
     vm.stopBroadcast();
   }
-}
-
-// BROADCAST
-// source .env && forge script MockSetupPostEnvironment --with-gas-price 2000000000 -vvvvv --rpc-url $ARB_SEPOLIA_RPC --broadcast --verify --etherscan-api-key $ARB_ETHERSCAN_API_KEY
-
-// SIMULATE
-// source .env && forge script MockSetupPostEnvironment --with-gas-price 2000000000 -vvvvv --rpc-url $ARB_SEPOLIA_RPC
-
-contract MockSetupPostEnvironment is Common {
-  IAlgebraFactory public algebraFactory = IAlgebraFactory(SEPOLIA_ALGEBRA_FACTORY);
-
-  function run() public {
-    vm.startBroadcast(vm.envUint('ARB_SEPOLIA_DEPLOYER_PK'));
-    MintableERC20 mockWeth = new MintableERC20('Wrapped ETH', 'WETH', 18);
-
-    algebraFactory.createPool(SEPOLIA_SYSTEM_COIN, address(mockWeth));
-    address _pool = algebraFactory.poolByPair(SEPOLIA_SYSTEM_COIN, address(mockWeth));
-
-    uint160 _sqrtPriceX96 = initialPrice(INIT_OD_AMOUNT, INIT_WETH_AMOUNT, _pool);
-    IAlgebraPool(_pool).initialize(uint160(_sqrtPriceX96));
-
-    IBaseOracle _odWethOracle = camelotRelayerFactory.deployAlgebraRelayer(
-      SEPOLIA_ALGEBRA_FACTORY, SEPOLIA_SYSTEM_COIN, address(mockWeth), uint32(ORACLE_INTERVAL_TEST)
-    );
-
-    IBaseOracle chainlinkEthUSDPriceFeed =
-      chainlinkRelayerFactory.deployChainlinkRelayer(SEPOLIA_CHAINLINK_ETH_USD_FEED, ORACLE_INTERVAL_TEST);
-
-    // deploy systemOracle
-    denominatedOracleFactory.deployDenominatedOracle(_odWethOracle, chainlinkEthUSDPriceFeed, false);
-
-    authOnlyFactories();
-
-    vm.stopBroadcast();
 }
 
 // BROADCAST
