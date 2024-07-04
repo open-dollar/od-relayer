b75810b78ccfb8a7493c96401bcd90b35e55c336
Patrick Gallagher <blockchainbuddha@gmail.com>
2024-07-03 19:07:37 -0700

    6/7/24 deployment

--- script/DeployOracle.s.sol
+++ script/DeployOracle.s.sol
@@ -4,6 +4,7 @@ pragma solidity 0.7.6;
 import '@script/Registry.s.sol';
 import {Script} from 'forge-std/Script.sol';
 import {CommonMainnet} from '@script/Common.s.sol';
+import 'forge-std/console2.sol';
 
 import {CamelotRelayerFactory} from '@contracts/factories/CamelotRelayerFactory.sol';
 import {ChainlinkRelayerFactory} from '@contracts/factories/ChainlinkRelayerFactory.sol';
@@ -14,14 +15,17 @@ import {IBaseOracle} from '@interfaces/oracles/IBaseOracle.sol';
 // source .env && forge script DeployEthUsdRelayer --with-gas-price 2000000000 -vvvvv --rpc-url $ARB_MAINNET_RPC --broadcast --verify --etherscan-api-key $ARB_ETHERSCAN_API_KEY --account defaultKey --sender $DEFAULT_KEY_PUBLIC_ADDRESS
 
 // SIMULATE
-// source .env && forge script DeployEthUsdRelayer --with-gas-price 2000000000 -vvvvv --rpc-url $ARB_MAINNET_RPC --account defaultKey
+// source .env && forge script DeployEthUsdRelayer --with-gas-price 2000000000 -vvvvv --rpc-url $ARB_MAINNET_RPC --sender $DEFAULT_KEY_PUBLIC_ADDRESS
 
 contract DeployEthUsdRelayer is Script, CommonMainnet {
   function run() public {
     vm.startBroadcast();
 
     chainlinkRelayerFactory.deployChainlinkRelayerWithL2Validity(
-      MAINNET_CHAINLINK_ETH_USD_FEED, MAINNET_CHAINLINK_SEQUENCER_FEED, MAINNET_ORACLE_INTERVAL, MAINNET_GRACE_PERIOD
+      MAINNET_CHAINLINK_ETH_USD_FEED,
+      MAINNET_CHAINLINK_SEQUENCER_FEED,
+      1 days,
+      MAINNET_CHAINLINK_L2VALIDITY_GRACE_PERIOD
     );
 
     vm.stopBroadcast();
@@ -32,7 +36,7 @@ contract DeployEthUsdRelayer is Script, CommonMainnet {
 // source .env && forge script DeployLinkGrtEthOracles --with-gas-price 2000000000 -vvvvv --rpc-url $ARB_MAINNET_RPC --broadcast --verify --etherscan-api-key $ARB_ETHERSCAN_API_KEY --account defaultKey --sender $DEFAULT_KEY_PUBLIC_ADDRESS
 
 // SIMULATE
-// source .env && forge script DeployLinkGrtEthOracles --with-gas-price 2000000000 -vvvvv --rpc-url $ARB_MAINNET_RPC --account defaultKey
+// source .env && forge script DeployLinkGrtEthOracles --with-gas-price 2000000000 -vvvvv --rpc-url $ARB_MAINNET_RPC --sender $DEFAULT_KEY_PUBLIC_ADDRESS
 
 contract DeployLinkGrtEthOracles is Script, CommonMainnet {
   IBaseOracle public _linkUSDRelayer;
@@ -43,17 +47,27 @@ contract DeployLinkGrtEthOracles is Script, CommonMainnet {
     vm.startBroadcast();
 
     _linkUSDRelayer = chainlinkRelayerFactory.deployChainlinkRelayerWithL2Validity(
-      MAINNET_CHAINLINK_LINK_USD_FEED, MAINNET_CHAINLINK_SEQUENCER_FEED, MAINNET_ORACLE_INTERVAL, MAINNET_GRACE_PERIOD
+      MAINNET_CHAINLINK_LINK_USD_FEED,
+      MAINNET_CHAINLINK_SEQUENCER_FEED,
+      1 hours,
+      MAINNET_CHAINLINK_L2VALIDITY_GRACE_PERIOD
     );
     _grtUSDRelayer = chainlinkRelayerFactory.deployChainlinkRelayerWithL2Validity(
-      MAINNET_CHAINLINK_LINK_USD_FEED, MAINNET_CHAINLINK_SEQUENCER_FEED, MAINNET_ORACLE_INTERVAL, MAINNET_GRACE_PERIOD
+      MAINNET_CHAINLINK_GRT_USD_FEED,
+      MAINNET_CHAINLINK_SEQUENCER_FEED,
+      1 days,
+      MAINNET_CHAINLINK_L2VALIDITY_GRACE_PERIOD
     );
 
-    delayedOracleFactory.deployDelayedOracle(_linkUSDRelayer, MAINNET_ORACLE_INTERVAL);
-    delayedOracleFactory.deployDelayedOracle(_grtUSDRelayer, MAINNET_ORACLE_INTERVAL);
-    delayedOracleFactory.deployDelayedOracle(
-      IBaseOracle(MAINNET_CHAINLINK_L2VALIDITY_ETH_USD_RELAYER), MAINNET_ORACLE_INTERVAL
+    IBaseOracle linkOracle = delayedOracleFactory.deployDelayedOracle(_linkUSDRelayer, MAINNET_ORACLE_DELAY);
+    IBaseOracle grtOracle = delayedOracleFactory.deployDelayedOracle(_grtUSDRelayer, MAINNET_ORACLE_DELAY);
+    IBaseOracle ethOracle = delayedOracleFactory.deployDelayedOracle(
+      IBaseOracle(MAINNET_CHAINLINK_L2VALIDITY_ETH_USD_RELAYER), MAINNET_ORACLE_DELAY
     );
+
+    linkOracle.getResultWithValidity();
+    grtOracle.getResultWithValidity();
+    ethOracle.getResultWithValidity();
 
     vm.stopBroadcast();
   }
