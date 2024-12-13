443c9ba824adb357119a0c8a938e8c976e52974f
Patrick Gallagher <blockchainbuddha@gmail.com>
2024-12-12 20:53:53 -0800

    very unreliable twap

--- src/contracts/oracles/CamelotV2Relayer.sol
+++ src/contracts/oracles/CamelotV2Relayer.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.7.6;
 
+import '@cryptoalgebra/v1.9-core/contracts/libraries/FullMath.sol';
+import '@cryptoalgebra/v1.9-core/contracts/libraries/LowGasSafeMath.sol';
+
 import {IERC20Metadata} from '@algebra-periphery/interfaces/IERC20Metadata.sol';
-import {IDataStorageOperator} from '@algebra-core/interfaces/IDataStorageOperator.sol';
-import {DataStorageLibrary} from '@algebra-periphery/libraries/DataStorageLibrary.sol';
 import {ICamelotFactory} from '@interfaces/camelot/ICamelotFactory.sol';
 import {ICamelotPair} from '@interfaces/camelot/ICamelotPair.sol';
 
@@ -19,6 +20,11 @@ contract CamelotV2Relayer {
 
   // --- Data ---
   string public symbol;
+
+  // TWAP state variables
+  uint256[] public priceHistory; // Array to store historical prices
+  uint256[] public timestampHistory; // Array to store corresponding timestamps
+  uint256 public constant MAX_HISTORY_LENGTH = 100; // Maximum number of price entries to store
 
   constructor(address _camelotV2Factory, address _baseToken, address _quoteToken, uint32 _quotePeriod) {
     camelotV2Pair = ICamelotFactory(_camelotV2Factory).getPair(_baseToken, _quoteToken);
@@ -50,42 +56,64 @@ contract CamelotV2Relayer {
   }
 
   function getResultWithValidity() external view returns (uint256 _result, bool _validity) {
-    uint112 _reserve0;
-    uint112 _reserve1;
-    (_reserve0, _reserve1,,) = ICamelotPair(camelotV2Pair).getReserves();
+    updatePrice();
 
-    require(_reserve0 > 0 && _reserve1 > 0, 'CamelotPair: INSUFFICIENT_RESERVES');
-
-    uint256 price;
-    if (baseToken == ICamelotPair(camelotV2Pair).token0()) {
-      // baseToken is token0, quoteToken is token1
-      price = uint256(_reserve1) * BASE_AMOUNT / uint256(_reserve0);
-    } else {
-      // baseToken is token1, quoteToken is token0
-      price = uint256(_reserve0) * BASE_AMOUNT / uint256(_reserve1);
-    }
+    // TODO
 
     _result = _parseResult(price);
     _validity = true;
   }
 
   function read() external view returns (uint256 _result) {
-    uint112 _reserve0;
-    uint112 _reserve1;
-    (_reserve0, _reserve1,,) = ICamelotPair(camelotV2Pair).getReserves();
+    _result = getCurrentPrice();
+  }
+
+  function getCurrentPrice() internal view returns (uint256) {
+    uint112 reserve0;
+    uint112 reserve1;
+    (reserve0, reserve1,,) = ICamelotPair(camelotV2Pair).getReserves();
 
     require(_reserve0 > 0 && _reserve1 > 0, 'CamelotPair: INSUFFICIENT_RESERVES');
 
     uint256 price;
     if (baseToken == ICamelotPair(camelotV2Pair).token0()) {
       // baseToken is token0, quoteToken is token1
-      price = uint256(_reserve1) * BASE_AMOUNT / uint256(_reserve0);
+      price = FullMath.mulDiv(reserve1, BASE_AMOUNT, reserve0);
     } else {
       // baseToken is token1, quoteToken is token0
-      price = uint256(_reserve0) * BASE_AMOUNT / uint256(_reserve1);
+      price = FullMath.mulDiv(reserve0, BASE_AMOUNT, reserve1);
     }
 
-    _result = _parseResult(price);
+    return _parseResult(price);
+  }
+
+  // Function to calculate the TWAP
+  function getTWAP(uint32 period) external view returns (uint256 twap) {
+    uint256 totalWeightedPrice = 0;
+    uint256 totalTime = 0;
+
+    for (uint256 i = 0; i < priceHistory.length; i++) {
+      if (block.timestamp - timestampHistory[i] <= period) {
+        totalWeightedPrice += priceHistory[i] * (block.timestamp - timestampHistory[i]);
+        totalTime += (block.timestamp - timestampHistory[i]);
+      }
+    }
+
+    require(totalTime > 0, 'No price data available for the specified period');
+    twap = totalWeightedPrice / totalTime;
+  }
+
+  function _updatePrice() internal {
+    uint256 currentPrice = getCurrentPrice();
+    uint256 currentTime = block.timestamp;
+
+    priceHistory.push(currentPrice);
+    timestampHistory.push(currentTime);
+
+    if (priceHistory.length > MAX_HISTORY_LENGTH) {
+      priceHistory.pop();
+      timestampHistory.pop();
+    }
   }
 
   function _parseResult(uint256 _quoteResult) internal view returns (uint256 _result) {
